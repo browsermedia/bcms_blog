9efee296a4877294204e79052b3f35cf9e514616
Jon Leighton <j@jonathanleighton.com>
2009-07-24 17:46:03 +0100

    Prevent routes being loaded by a 'host' application

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
-ActionController::Routing::Routes.draw do |map|
-  map.routes_for_bcms_blog
-  map.routes_for_browser_cms
+# Only load these routes if this is the "root" application
+if RAILS_ROOT == File.expand_path(File.dirname(__FILE__) + "/..")
+  ActionController::Routing::Routes.draw do |map|
+    map.routes_for_bcms_blog
+    map.routes_for_browser_cms
+  end
 end
