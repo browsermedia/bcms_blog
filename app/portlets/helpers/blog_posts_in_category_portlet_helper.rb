9b3c4133ac58aa039d8743f68fac3042a2d94218
Tyler Rick <github.com@tylerrick.com>
2010-04-26 19:31:15 -0700

    Removing 5 portlets that don't appear to be necessary and create unnecessary duplication. We can simply handle the searching that these portlets are designed to handle directly in the Blog model's render method.

--- app/portlets/helpers/blog_posts_in_category_portlet_helper.rb
+++
@@ -1,5 +0,0 @@
-##
-# All methods from this helper will be available in the render.html.erb for BlogPostsInCategoryPortlet
-module BlogPostsInCategoryPortletHelper
-  
-end
--- app/portlets/helpers/blog_posts_with_tag_portlet_helper.rb
+++
@@ -1,5 +0,0 @@
-##
-# All methods from this helper will be available in the render.html.erb for BlogPostsWithTagPortlet
-module BlogPostsWithTagPortletHelper
-  
-end
