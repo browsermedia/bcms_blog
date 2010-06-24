0b961833922da2f376966b3a8cc58cd119b8be75
Juan Alvarez <alce@mac.com>
2010-06-24 01:57:33 -0500

    RSS feeds implementation

--- lib/bcms_blog/routes.rb
+++ lib/bcms_blog/routes.rb
@@ -1,5 +1,8 @@
 module Cms::Routes
   def routes_for_bcms_blog
+    
+    blog_feeds '/blog/feeds', :controller => "feeds", :format => "rss"
+    
     namespace(:cms) do |cms|      
       cms.content_blocks :blogs
       cms.content_blocks :blog_posts
