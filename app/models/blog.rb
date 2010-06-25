ade79f926fa9bc18b9fdcdca3583be06e10cbc94
Juan Alvarez <alce@mac.com>
2010-06-25 01:58:53 -0500

    Slight refactoring of Blog model. Added some comments

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,14 +1,15 @@
 class Blog < ActiveRecord::Base
   acts_as_content_block
+  
   has_many :posts, :class_name => "BlogPost", :conditions => { :published => true }, :order => "published_at desc"
-
   has_many :blog_group_memberships
   has_many :groups, :through => :blog_group_memberships
 
   validates_presence_of :name
   validates_uniqueness_of :name
   
-  before_update :update_section_pages_and_route
+  before_update :update_section_pages_and_route 
+  after_create  :create_section_pages_and_routes
 
   named_scope :editable_by, lambda { |user|
     if user.able_to?(:administrate)
@@ -90,53 +91,80 @@ class Blog < ActiveRecord::Base
     groups.map(&:users).flatten.uniq
   end
 
-
-  #-------------------------------------------------------------------------------------------------
-
   def name_for_path
     name.to_slug.gsub('-', '_')
   end
 
-protected
-  def after_create
-    section = Section.find_by_name(name) || (
-      section = Section.create!(
+  protected
+  
+  # A section, two pages, 6 routes and a portlet are created alongside every blog.
+  # This structure provides sensible defaults so users can pretty much start adding
+  # posts right after creating a blog without having to worry about where to put
+  # their blog and portlets.
+  
+  def create_section_pages_and_routes
+    create_blog_section
+    create_blog_block_page
+    create_post_portlet_page
+    reload_routes
+  end
+  
+  # Every blog is created within a section with the same name.
+  # For example, if you create a blog named 'MyBlog', a section 'MyBlog' will be
+  # created. This section will hold two pages: one for the blog ContentBlock that
+  # will render the list of posts and one for the BlogPost portlet (ie the individual
+  # post view)  
+  
+  def create_blog_section
+    @section = Section.find_by_name(name) || (
+      @section = Section.create!(
         :name => name,
         :path => "/#{name_for_path}",
         :parent_id => 1
       )
-      section.groups << Group.find_by_code("cms-admin")
-      section.groups << Group.find_by_code("guest")
-      section.groups << Group.find_by_code("content-editor")
-      section.save!
-      section
+      @section.groups << Group.find_by_code("cms-admin")
+      @section.groups << Group.find_by_code("guest")
+      @section.groups << Group.find_by_code("content-editor")
+      @section.save!
+      @section
     )
-
+  end
+  
+  # Following with the above example, the first page that is created is named 'MyBlog' and
+  # holds the Blog ContentBlock directly, not a portlet. Together with the 5 created routes,
+  # this page and its  ContentBlock handle different post listings (all posts, posts in year, 
+  # month or day and posts by tag or category).
+  
+  def create_blog_block_page
     page = Page.find_by_name(name) || Page.create!(
       :name => name,
       :path => "/#{name_for_path}",
-      :section => section,
+      :section => @section,
       :template_file_name => "default.html.erb",
       :hidden => true
     )
     page.create_connector(self, 'main')
+    
     create_route(page, "#{name}: Posts In Day",      "/#{name_for_path}/:year/:month/:day")
     create_route(page, "#{name}: Posts In Month",    "/#{name_for_path}/:year/:month")
     create_route(page, "#{name}: Posts In Year",     "/#{name_for_path}/:year")
     create_route(page, "#{name}: Posts With Tag",    "/#{name_for_path}/tag/:tag")
     create_route(page, "#{name}: Posts In Category", "/#{name_for_path}/category/:category")
-
+  end
+  
+  # The second page that is created holds the BlogPostPortlet and displays the individual
+  # post view, along with it's comments.
+  
+  def create_post_portlet_page
     page = Page.find_by_name(portlet_name = "#{name}: Post") || Page.create!(
       :name => portlet_name,
       :path => "/#{name_for_path}/post",
-      :section => section,
+      :section => @section,
       :template_file_name => "default.html.erb",
       :hidden => true)
     page.publish
-    create_route(  page, portlet_name, "/#{name_for_path}/:year/:month/:day/:slug")
+    create_route(page, portlet_name, "/#{name_for_path}/:year/:month/:day/:slug")
     create_portlet(page, portlet_name, BlogPostPortlet)
-    
-    reload_routes
   end
   
   # When the name of a Blog block changes, we need to change the Post page route.
@@ -154,6 +182,10 @@ protected
       [PageRoute, Page].each { |k| k.find_by_name("#{old_blog_name}: Post").update_attribute(:name, "#{name}: Post") }
       [Section,   Page].each { |k| k.find_by_name(old_blog_name).update_attribute(:name, name) }
     end
+  end 
+  
+  def reload_routes
+     ActionController::Routing::Routes.load!
   end
 
   def create_route(page, name, pattern)
@@ -173,9 +205,5 @@ protected
       :connect_to_page_id => page.id,
       :connect_to_container => "main",
       :publish_on_save => true)
-  end
-  
-  def reload_routes
-     ActionController::Routing::Routes.load!
   end
 end
