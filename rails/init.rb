604a0a91db1b2526ec32d72c71436bf1c71d8b51
crh3675 <crh3675@gmail.com>
2011-10-28 11:01:57 -0300

    Update rails/init.rb

--- rails/init.rb
+++ rails/init.rb
@@ -1,7 +1,10 @@
 gem_root = File.expand_path(File.join(File.dirname(__FILE__), ".."))
 Cms.add_to_rails_paths gem_root
 Cms.add_generator_paths gem_root, "db/migrate/[0-9]*_*.rb"
-ApplicationHelper.module_eval { include Cms::BlogHelper }
+# Doesn't load helpers in some Rails 2 versions, use alternate code
+# crh 2011-10-28
+# ApplicationHelper.module_eval { include Cms::BlogHelper }
+ActionView::Base.send :include, Cms::BlogHelper
 
 config.after_initialize do
   ActiveRecord::Base.observers << BlogObserver
