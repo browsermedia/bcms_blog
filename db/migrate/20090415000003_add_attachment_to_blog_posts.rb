4f95287aeb1f7ddb06d4474e4fd8a4c1c3c29ee2
Jim <jdurand@o2web.ca>
2013-07-23 11:36:09 -0400

    Fixes for BrowserCMS 3.5.x

--- db/migrate/20090415000003_add_attachment_to_blog_posts.rb
+++ db/migrate/20090415000003_add_attachment_to_blog_posts.rb
@@ -1,21 +1,21 @@
 class AddAttachmentToBlogPosts < ActiveRecord::Migration
   def self.up
-    change_table :blog_posts do |t|
+    change_table :cms_blog_posts do |t|
       t.belongs_to :attachment
       t.integer :attachment_version
     end
-    change_table :blog_post_versions do |t|
+    change_table :cms_blog_post_versions do |t|
       t.belongs_to :attachment
       t.integer :attachment_version
     end
   end
 
   def self.down
-    change_table :blog_posts do |t|
+    change_table :cms_blog_posts do |t|
       t.remove :attachment
       t.remove :attachment_version
     end
-    change_table :blog_post_versions do |t|
+    change_table :cms_blog_post_versions do |t|
       t.remove :attachment
       t.remove :attachment_version
     end
--- db/migrate/20120529184028_v130.rb
+++ db/migrate/20120529184028_v130.rb
@@ -3,6 +3,14 @@ require 'cms/upgrades/v3_5_0'
 # Upgrade this module to v1.3.0 of BcmsBlog (Rails 3.2/CMS 3.5.x compatible)
 class V130 < ActiveRecord::Migration
   def change
+
+    rename_table "cms_blogs", "blogs"
+    rename_table "cms_blog_versions", "blog_versions"
+    rename_table "cms_blog_posts", "blog_posts"
+    rename_table "cms_blog_post_versions", "blog_post_versions"
+    rename_table "cms_blog_comments", "blog_comments"
+    rename_table "cms_blog_comment_versions", "blog_comment_versions"
+
     ["Blog", "BlogPost", "BlogComment"].each do |model|
       v3_5_0_apply_namespace_to_block("BcmsBlog", model)
     end
