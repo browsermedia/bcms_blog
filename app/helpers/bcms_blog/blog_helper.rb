27a7bdf48a0844757142c31f7f2fc5db9dc79dc5
Patrick Peak <peakpg@gmail.com>
2013-11-22 14:38:23 -0500

    Fix BrowserCMS issue #562

--- app/helpers/bcms_blog/blog_helper.rb
+++ app/helpers/bcms_blog/blog_helper.rb
@@ -6,7 +6,7 @@ module BcmsBlog
     end
 
     def _blog_post_path(blog_post)
-      main_app.send("#{blog_post.route_name}_path", blog_post.route_params)
+      send("#{blog_post.route_name}_path", blog_post.route_params)
     end
   
     def feeds_link_tag_for(name)
@@ -17,11 +17,6 @@ module BcmsBlog
     def new_comment_params(portlet)
       {:url=> Cms::Engine.routes.url_helpers.portlet_handler_path(:id=>portlet.id, :handler=>'create_comment'), 
       :method=>'post'}
-    end
-    
-    # Cms::ViewContext doesn't expose this method correctly, so duplicating it here.
-    def main_app
-      Rails.application.routes.url_helpers
     end
   end
 end
